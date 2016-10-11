eb297e2d61470c367f283e8f188f6db578a208d4
wang <trackerwjw@outlook.com>
2016-10-11 15:34:01 +0800

    java and scala

---
+++ src/com/us/scala/TEST.java
@@ -0,0 +1,14 @@
+package com.us.scala;
+
+/**
+ * Created by kevin on 16-10-11-下午3:28-下午3:33.
+ */
+public class TEST {
+
+    public static void main(String[] args){
+        System.out.println("asd");
+        Test3 c = new Test3();
+        System.out.println(c.myage());
+    }
+
+}
--- src/com/us/scala/Test2.scala
+++ src/com/us/scala/Test2.scala
@@ -3,6 +3,16 @@ package com.us.scala
 /**
   * Created by hadoop123 on 16-10-11.
   */
+
+class Test3{
+  val age = 5
+  def myage = age
+}
 object Test2 {
 
+  def main(args: Array[String]): Unit = {
+
+  }
+
 }
+
